49dd976bc9ecb42d3a8b9965e7b4d82535e6bbfd
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-10-02 22:37:11 +0200

    first version of leaderboard

---
+++ hacktoberfest/hacktoberfest-leaderboard.groovy
@@ -0,0 +1,46 @@
+import groovy.json.*
+def members = new File('members.txt').text
+entry = [:]
+def template = """
+      <entry>
+        <avatar>
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+            <defs>
+               <pattern id="image-bg-${->entry.name}" x="0" y="0" height="100" width="100" patternUnits="userSpaceOnUse">
+                 <image width="100" height="100" xlink:href="${->entry.userImage}"></image>
+              </pattern>
+            </defs>
+            
+            <polygon class="hex" points="100,50 75,93 25,93 0,50 25,6.6 75,6.6" fill="url('#image-bg-${->entry.name}')"></polygon>
+          </svg>
+        </avatar>
+        <name><a href="https://github.com/${->entry.name}">${->entry.name}</a></name>
+        <prs>${->entry.prs}</prs>
+      </entry>
+"""
+def board = []
+members.eachLine { member ->
+    if (member.trim()) {
+        def pullRequests = new URL("https://hacktoberfestchecker.jenko.me/prs?username="+member).text
+        pullRequests = new JsonSlurper().parseText(pullRequests)
+        println "-"*80
+        println member
+        println pullRequests.userImage
+        pullRequests.prs.each { pr ->
+            println pr.repo_name
+            println pr.created_at
+        }
+        board<<[name: member, prs:pullRequests.prs.size(), userImage: pullRequests.userImage]
+    }
+}
+def html = ""
+board.sort {it.prs}.reverse().each { currentEntry ->
+    entry = currentEntry
+    html += template
+}
+new File("build/.").mkdirs()
+new File("build/leaderboard.html").write(
+    new File("leaderboard.template").text.replaceAll("%content%",html)
+    )
+return 0
