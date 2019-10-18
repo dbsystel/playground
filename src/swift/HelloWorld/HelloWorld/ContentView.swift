1d18b0fe7f48c56a0bea99167cd76ab83856fb4c
Niklas Holloh <git@niklasholloh.de>
2019-10-18 21:53:24 +0200

    Added hello world project in swift

---
+++ src/swift/HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Niklas Holloh on 18.10.19.
+//  Copyright © 2019 Niklas Holloh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
